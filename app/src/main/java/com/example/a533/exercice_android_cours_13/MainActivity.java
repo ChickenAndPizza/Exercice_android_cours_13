1afb9ff62fecce353575f41732ebebdae99fec3b
B533P-20\533 <alessard97@hotmail.com>
2019-04-25 12:41:30 -0400

    Création projet

---
+++ app/src/main/java/com/example/a533/exercice_android_cours_13/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a533.exercice_android_cours_13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
